3bd82adb1e8b5eec4d1e65f1d1aaaa18cfc36ea2
miri-hoptayl <m0548534682@gmail.com>
2025-06-27 11:57:01 +0300

    Initial commit of full project

---
+++ Backend.CORE/DTO/UsersDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Backend.CORE.DTO
+{
+    public class UsersDTO
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public int Age { get; set; }
+    }
+}
---
+++ Backend.CORE/entities/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.CORE.entities
+{
+    public class Users
+    {
+        public int Id { get; set; }
+    
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string ?Email { get; set; }
+        public int Age { get; set; }
+        public int AgeGoupId { get; set; } //צריך להגדיר FK
+        public int TotalPoints { get; set; }    
+        public int Level { get; set; }
+        public string? Role { get; set; } = "user";
+        public DateTime CraetedTime { get; set; }
+
+    }
+}
